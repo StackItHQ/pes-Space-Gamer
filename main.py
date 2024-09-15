5a4da658a3f634ff7c62110f8f3c175dbdc3727e
Space_Gamer <spacegamer1111@gmail.com>
2024-09-15 13:08:47 +0530

    More detailed skeleton
    + Added the flow in which the updates must be checked and conflicts resolved.

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ db_user = getenv("DB_USER")
 db_pass = getenv("DB_PASS")
 db_name = getenv("DB_NAME")
 update_interval = int(getenv("UPDATE_INTERVAL"))
+conflict_priority = getenv("CONFLICT_PRIORITY")
 
 # Connect to the Google Sheets API
 try:
@@ -52,11 +53,52 @@ except psycopg2.OperationalError:
 
 cur = conn.cursor()
 
+# Last updated time of the sheet
+last_sheet_update = 0
+last_db_update = 0
+
 # Start syncronisation
 logger.info("Starting syncronisation")
 try:
     while True:
-        # Do processing here
+        # Get the updated version of the sheet
+        sh = gc.open(sheet)
+        sh_updated = sh.lastUpdateTime
+        print("Sheet updated at: ", sh_updated)
+
+        # Get the  updated version of the database
+        db_updated = cur.execute("SELECT max(last_updated) FROM candidates")
+        db_updated = cur.fetchone()[0]
+
+        db_deleted = cur.execute("SELECT max(last_updated) FROM deleted_candidates")
+        db_deleted = cur.fetchone()[0]
+
+        if db_updated and db_deleted:
+            db_updated = max(db_updated, db_deleted)
+        elif db_deleted:
+            db_updated = db_deleted
+        print("Database updated at: ", db_updated)
+
+        if sh_updated != last_sheet_update and last_db_update == db_updated:    # If only the sheet has been updated since the last update
+            logger.info("Sheet has been updated")
+            # Do processing here
+        elif sh_updated == last_sheet_update and last_db_update != db_updated:  # If only the database has been updated since the last update
+            logger.info("Database has been updated")
+            # Do processing here
+        elif sh_updated != last_sheet_update and last_db_update != db_updated:  # If both the sheet and the database have been updated since the last update
+            logger.info("Sheet and database have been updated")
+            if conflict_priority == "Sheet":
+                # Do processing here
+                pass
+            else:
+                # Do processing here
+                pass
+        else:                                                                   # No updates
+            pass
+
+        # Update the last updated times
+        last_sheet_update = sh_updated
+        last_db_update = db_updated
 
         # Sleep for the update interval
         time.sleep(update_interval)
