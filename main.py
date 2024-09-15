0fd338db758e7f8b7c3e9eb4fa53232806c3b821
Space_Gamer <spacegamer1111@gmail.com>
2024-09-15 18:59:30 +0530

    Implemented syncing of data
    + Can sync in all three conditions: just sheet modification, just db modification, both sheet and db modifications

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import traceback
 from os import getenv
 
 import gspread
@@ -10,7 +11,7 @@ from dotenv import load_dotenv
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 handler = logging.FileHandler("logs.log", encoding='utf-8', mode='a')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
 logger.addHandler(handler)
 
 # Load environment variables
@@ -57,6 +58,17 @@ cur = conn.cursor()
 last_sheet_update = 0
 last_db_update = 0
 
+def check_row(sh_row, db_row):
+    if sh_row['First Name'] != db_row[1]:
+        return False
+    if sh_row['Last Name'] != db_row[2]:
+        return False
+    if sh_row['Email'] != db_row[3]:
+        return False
+    if sh_row['Phone Number'] != db_row[4]:
+        return False
+    return True
+
 # Start syncronisation
 logger.info("Starting syncronisation")
 try:
@@ -81,18 +93,71 @@ try:
 
         if sh_updated != last_sheet_update and last_db_update == db_updated:    # If only the sheet has been updated since the last update
             logger.info("Sheet has been updated")
-            # Do processing here
+            # Get the data from the sheet and the database
+            sh_data = sh.sheet1.get_all_records()[1:]
+            db_data = cur.execute("SELECT * FROM candidates")
+            db_data = cur.fetchall()
+            
+            for i,row in enumerate(sh_data):
+                if i < len(db_data) and not check_row(row, db_data[i]):
+                    # Update the database
+                    cur.execute("UPDATE candidates SET first_name = %s, last_name = %s, email = %s, phone = %s, last_updated = %s WHERE id = %s", 
+                                (row["First Name"], row["Last Name"], row["Email"], row["Phone Number"], sh_updated, i+1))
+                elif i >= len(db_data):
+                    cur.execute("INSERT INTO candidates (first_name, last_name, email, phone, last_updated) VALUES (%s, %s, %s, %s, %s)",
+                                (row["First Name"], row["Last Name"], row["Email"], row["Phone Number"], sh_updated))
+            if len(sh_data) < len(db_data):
+                for i in range(len(sh_data), len(db_data)):
+                    cur.execute("DELETE FROM candidates WHERE id = %s", (db_data[i]["id"],))
+            conn.commit()
         elif sh_updated == last_sheet_update and last_db_update != db_updated:  # If only the database has been updated since the last update
             logger.info("Database has been updated")
-            # Do processing here
+            # Get the data from the sheet and the database
+            sh_data = sh.sheet1.get_all_records()[1:]
+            db_data = cur.execute("SELECT * FROM candidates")
+            db_data = cur.fetchall()
+
+            min_len = min(len(sh_data), len(db_data))
+            update_data = [[row[1], row[2], row[3], row[4]] for row in db_data[:min_len]]
+            sh.sheet1.update(update_data, f"A2:D{min_len+1}")
+            if len(sh_data) < len(db_data):  # If there are more rows in the database than in the sheet
+                sh.sheet1.insert_rows([[row[1], row[2], row[3], row[4]] for row in db_data[min_len:]], min_len+2)
+            elif len(sh_data) > len(db_data):
+                sh.sheet1.delete_rows(len(db_data)+2, len(sh_data)-len(db_data))
+
         elif sh_updated != last_sheet_update and last_db_update != db_updated:  # If both the sheet and the database have been updated since the last update
             logger.info("Sheet and database have been updated")
             if conflict_priority == "Sheet":
-                # Do processing here
-                pass
+                sh_data = sh.sheet1.get_all_records()[1:]
+                db_data = cur.execute("SELECT * FROM candidates")
+                db_data = cur.fetchall()
+
+                for i,row in enumerate(sh_data):
+                    if i < len(db_data) and not check_row(row, db_data[i]):
+                        # Update the database
+                        cur.execute("UPDATE candidates SET first_name = %s, last_name = %s, email = %s, phone = %s, last_updated = %s WHERE id = %s",
+                                    (row["First Name"], row["Last Name"], row["Email"], row["Phone Number"], sh_updated, i+1))
+                    elif i >= len(db_data):
+                        cur.execute("INSERT INTO candidates (first_name, last_name, email, phone, last_updated) VALUES (%s, %s, %s, %s, %s)",
+                                    (row["First Name"], row["Last Name"], row["Email"], row["Phone Number"], sh_updated))
+                if len(sh_data) < len(db_data):
+                    for i in range(len(sh_data), len(db_data)):
+                        cur.execute("DELETE FROM candidates WHERE id = %s", (db_data[i]["id"],))
+                conn.commit()
+
             else:
-                # Do processing here
-                pass
+                sh_data = sh.sheet1.get_all_records()[1:]
+                db_data = cur.execute("SELECT * FROM candidates")
+                db_data = cur.fetchall()
+
+                min_len = min(len(sh_data), len(db_data))
+                update_data = [[row[1], row[2], row[3], row[4]] for row in db_data[:min_len]]
+                sh.sheet1.update(update_data, f"A2:D{min_len+1}")
+                if len(sh_data) < len(db_data):
+                    sh.sheet1.insert_rows([[row[1], row[2], row[3], row[4]] for row in db_data[min_len:]], min_len+2)
+                elif len(sh_data) > len(db_data):
+                    sh.sheet1.delete_rows(len(db_data)+2, len(sh_data)-len(db_data))
+            
         else:                                                                   # No updates
             pass
 
@@ -107,6 +172,6 @@ except KeyboardInterrupt:
     conn.close()
     exit(0)
 except Exception as e:
-    logger.error(f"An error occured: {e}")
+    logger.error(f"An error occured: {e}\n{traceback.format_exc()}")
     conn.close()
     exit(1)
